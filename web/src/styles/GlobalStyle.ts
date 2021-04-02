8e9421f94447250d18e261efc0618bd07093b8d1
x14102004x <marcosproenca144@gmail.com>
2021-04-01 21:07:56 -0300

    Added: global icons

---
+++ web/src/styles/GlobalStyle.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        appearance: none;
+        box-sizing: border-box; 
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    }
+    *::-webkit-scrollbar{
+        width: 30px;
+        height: 5px;
+        @media (max-width: 768px){
+            width: 5px;
+        }
+    }
+    *::-webkit-scrollbar-thumb{
+        border-radius: 50rem;
+        background-color: var(--rgba-0);
+    }
+    *{
+        scrollbar-color:var(--rgba-0) transparent;
+        @media (max-width: 768px){
+            scrollbar-width: thin;
+        }
+    }
+    html, body, #root{
+        height: 100%;
+        min-width: 260px;
+        min-height: 100vh;
+    }
+    ::selection{
+        background-color: var(--rgba-0);
+        color: var(--selection);
+    }
+    :root{
+        --selection: #fff;
+        --primary: #2c2e3e;
+        --secondary: #474a5f;
+        --text : #fff; 
+        --text-secondary: #8d99ae; 
+        --rgba-0: #2f9d;
+    }
+
+    #root{
+        width: 100%;
+        height: 100vh;
+        position: fixed;
+        overflow: hidden;
+        color: var(--text); 
+        background-color: var(--primary);
+    }
+
+    
+`
