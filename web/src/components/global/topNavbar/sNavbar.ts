27081fd8c4cb32fb0304069de99026238088d1ae
x14102004x <marcosproenca144@gmail.com>
2021-04-01 21:06:23 -0300

    Components: Global

---
+++ web/src/components/global/topNavbar/sNavbar.ts
@@ -0,0 +1,64 @@
+import styled, { css } from 'styled-components'
+import { Home, Android, Group2 } from '@styles/GlobalIcons'
+
+export const Wrapper = styled.div`
+   height: 100%;
+   width: 100%;
+   margin: 0 auto;
+   max-width: 1280px;
+   align-items: center;
+   justify-content: space-between;
+   display: flex;
+`
+export const Container = styled.div`
+   position: fixed;
+   left: 0;
+   top: 0;
+   z-index: 20;
+   height: 54px;
+   width: 100%;
+   background-color: var(--secondary);
+`
+export const Padding = styled.div`
+   height: 57px;
+`
+export const LogoIcon = styled(Android)`
+   color: var(--rgba-0);
+   width: 40px;
+   height: 40px;
+`
+export const NavbarN = styled.div`
+   padding: 5px 20px;
+   background-color: rgba(0, 0, 0, 0.2);
+   border-radius: 10px;
+`
+export const NavbarTab = styled.div`
+   display: flex;
+   list-style: none;
+`
+export const NavbarTabEl = styled.div`
+   font-weight: bold;
+   & > a {
+      text-decoration: none;
+      color: var(--text);
+   }
+   &.active {
+      border-bottom: 2px solid white;
+   }
+`
+const style = css`
+   width: 30px;
+   height: 30px;
+`
+export const Align = styled.div`
+   display: flex;
+   margin: 0 20px;
+   padding-bottom: 2px;
+   align-items: center;
+`
+export const HomeIcon = styled(Home)`
+   ${style}
+`
+export const GroupsIcon = styled(Group2)`
+   ${style}
+`
