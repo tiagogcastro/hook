2fb03b36fb8d8a28608e1134170ca507a8ad6507
x14102004x <marcosproenca144@gmail.com>
2021-04-01 21:11:52 -0300

    Restructured

--- src/Pages/Tweets/sTweets.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-    display: flex;
-    max-width: 1280px;
-    
-    
-`
--- src/Styles/Icons.ts
+++
@@ -1,4 +0,0 @@
-export { Android } from '@styled-icons/boxicons-logos/Android'
-export { SuitHeartFill, ShareFill } from '@styled-icons/bootstrap'
-export { Comment } from '@styled-icons/fa-solid/Comment'
-export { Videos, Group } from '@styled-icons/boxicons-solid'
