0c622641a806c559d199054ece01584b05aa9d42
Tiaguin061 <tiaguin180@gmail.com>
2021-04-05 16:22:50 -0300

    feat: Creating users routes

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+// import multer from 'multer';
+// import uploadConfig from '@config/upload';
+
+import UsersController from '../controllers/UsersController';
+
+// import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+// import UserAvatarController from '../controllers/UserAvatarController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+// const userAvatarController = new UserAvatarController();
+// const upload = multer(uploadConfig);
+
+usersRouter.post('/', usersController.create);
+
+export default usersRouter;
