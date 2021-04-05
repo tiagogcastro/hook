839c9b33e85b03c4aeb5971882899416648a389d
Tiaguin061 <tiaguin180@gmail.com>
2021-04-05 16:24:16 -0300

    feat: Creating class of errors

---
+++ src/shared/errors/index.ts
@@ -0,0 +1,13 @@
+type TStatusCode = '201 success' | '202 create' | '301 moved permanently' | '302 moved' | '400 bad request' | ' 401 unauthorized' | '404 not found' | '500 internal server error';
+
+export default class AppError {
+  public readonly message: string;
+  public readonly status: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, status: TStatusCode, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+    this.status = status;
+  }
+}
