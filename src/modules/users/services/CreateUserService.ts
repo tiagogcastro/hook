b233f6e8443b84cf1e20a81424090ccf80d78ace
Tiaguin061 <tiaguin180@gmail.com>
2021-04-06 14:05:52 -0300

    feat: Creating code to user Service

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,20 +1,43 @@
 import AppError from '@shared/errors';
-import { inject } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
+import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
   email: string;
-  name: string;
+  firstname: string;
   lastname: string;
   password: string;
+  username: string;
 }
+@injectable()
 class CreateUserService {
   constructor (
     @inject('UsersRepository')
-    private UsersRepository: IUsersRepository
+    private usersRepository: IUsersRepository
   ) {}
-  async execute({email, name, lastname, password}: IRequest): Promise<void> {
+  async execute({email, firstname, lastname, password, username}: IRequest): Promise<User> {
+    const existEmail = await this.usersRepository.findByEmail(email);
 
+    if(existEmail) {
+      throw new AppError('Este e-mail já existe. Por favor, informe outro', ' 401 unauthorized', 401);
+    }
+
+    const existUsername = await this.usersRepository.findByUsername(username);
+
+    if(existUsername) {
+      throw new AppError('Este username já existe. Por favor, informe outro', ' 401 unauthorized', 401);
+    }
+
+    const user = await this.usersRepository.create({
+      email,
+      firstname,
+      lastname,
+      password,
+      username: `@${username}`
+    });
+
+    return user
   }
 }
 
