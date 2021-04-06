91c7c88726d7a6df810d74a8240044fb02867ab4
Tiaguin061 <tiaguin180@gmail.com>
2021-04-06 14:06:19 -0300

    feat: Adding new methods

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,10 +1,48 @@
+import ICreateUserDto from '@modules/users/dtos/ICreateUserDTO';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { getRepository, Repository } from 'typeorm';
+import User from '../entities/User';
 
 // Criando os metodos do user usando typeorm
-class UsersRepository implements IUsersRepository{
-  async create(): Promise<void> {
 
+class UsersRepository implements IUsersRepository{
+  private ormRepository: Repository<User>
+
+  constructor() {
+    this.ormRepository = getRepository(User)
   }
+
+  async create(userData: ICreateUserDto): Promise<User> {
+    const user = this.ormRepository.create(userData);
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({
+      where: {email}
+    });
+
+    return user;
+  }
+
+  async findByid(id: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({
+      where: {id}
+    });
+
+    return user;
+  }
+
+  async findByUsername(username: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({
+      where: {username: `@${username}`}
+    });
+    return user;
+  }
+
 }
 
 export default UsersRepository;
--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -2,7 +2,11 @@
 // No typeorm só vai depender dessa interface.
 
 import ICreateUserDto from '../dtos/ICreateUserDTO';
+import User from '../infra/typeorm/entities/User';
 
 export default interface IUsersRepository {
-  create(data: ICreateUserDto): Promise<void>;
+  create(data: ICreateUserDto): Promise<User>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findByUsername(username: string): Promise<User | undefined>;
+  findByid(id: string): Promise<User | undefined>;
 }
