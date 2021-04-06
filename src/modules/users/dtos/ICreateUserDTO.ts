4ed9151f9cb44581c25e0ba12f8b53bc8b25413b
Tiaguin061 <tiaguin180@gmail.com>
2021-04-06 14:08:20 -0300

    fix: correcting variable name and adding username

--- src/modules/users/dtos/ICreateUserDTO.ts
+++ src/modules/users/dtos/ICreateUserDTO.ts
@@ -1,7 +1,8 @@
 // Cria as tipagens do metódo create para usar no repositories do typeorm e do ./repositories 
 export default interface ICreateUserDto {
   email: string;
-  name: string;
+  firstname: string;
   lastname: string;
   password: string;
+  username: string;
 }
