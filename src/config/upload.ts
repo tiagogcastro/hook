7edc224fdfa3ccf4fcd8b40940b831283f9ef0b3
Tiaguin061 <tiaguin180@gmail.com>
2021-04-07 18:35:14 -0300

    fix: Deleting variable in export default

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,18 +2,17 @@ import multer from 'multer'
 import path from 'path'
 import crypto from 'crypto'
 
-const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
 	tmpFolder,
-	uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 	storage: multer.diskStorage({
 		destination: tmpFolder,
 		filename: (req, file, callback) => {
 			const fileHash = crypto.randomBytes(10).toString('hex');
-			const fileName = `${fileHash}=${file.originalname}`
+			const fileName = `${fileHash}-${file.originalname}`;
 
-			callback(null, fileName)
+			callback(null, fileName);
 		}
 	})
 }
--- src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -8,17 +8,17 @@ class DiskStorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
     await fs.promises.rename(
       path.resolve(uploadConfig.tmpFolder, file),
-      path.resolve(uploadConfig.uploadsFolder, file)
+      path.resolve(uploadConfig.tmpFolder, file)
     );
 
     return file;
   }
 
   public async deleteFile(file: string): Promise<void> {
-    const filePath = path.resolve(uploadConfig.uploadsFolder, file);
+    const filePath = path.resolve(uploadConfig.tmpFolder, file);
 
     try {
-      await fs.promises.stat(filePath)
+      await fs.promises.stat(filePath);
     } catch {
       return;
     }
