fbe87822f751dfa8ea66f963192c19bd96efb769
x14102004x <marcosproenca144@gmail.com>
2021-04-07 09:20:59 -0300

    refactor: updated header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,76 @@
+import Centralize from 'components/Centralize';
 import Switch from 'components/Switch';
-import React from 'react';
-import { Container, Wrapper } from './styles';
+import { ToggleThemeContext } from 'contexts/ToggleThemeProvider';
+import React, { useContext } from 'react';
+import {
+  BiChat,
+  HiHome,
+  IoSearchOutline,
+  MdVideoLibrary,
+  TiGroup
+} from 'react-icons/all';
+import { Link } from 'react-router-dom';
+import {
+  Box,
+  Container,
+  LayerLeft,
+  LayerMiddle,
+  LayerRight,
+  Layers,
+  SearchBar
+} from './styles';
 
 const Header: React.FC = (): JSX.Element => {
+  const { theme } = useContext(ToggleThemeContext);
+
   return (
     <Container>
-      <Wrapper>
-        <div />
-        <Switch />
-      </Wrapper>
+      <Centralize>
+        <Layers>
+          <LayerLeft>
+            <button type="button">
+              <IoSearchOutline size={25} color={theme.colors.icons.primary} />
+            </button>
+            <SearchBar>
+              <input type="text" />
+            </SearchBar>
+          </LayerLeft>
+          <LayerMiddle>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">
+                    <HiHome size={25} color={theme.colors.icons.primary} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/groups">
+                    <TiGroup size={25} color={theme.colors.icons.primary} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/watch">
+                    <MdVideoLibrary
+                      size={25}
+                      color={theme.colors.icons.primary}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </LayerMiddle>
+          <LayerRight>
+            <Box>
+              <Link to="/messages">
+                <BiChat size={30} color={theme.colors.icons.primary} />
+              </Link>
+            </Box>
+            <Box>
+              <Switch />
+            </Box>
+          </LayerRight>
+        </Layers>
+      </Centralize>
     </Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,18 +1,54 @@
 import styled from 'styled-components';
 
-export const Container = styled.header`
-  height: 70px;
-  background-color: ${props => props.theme.colors.primary};
+export const Container = styled.div`
+  grid-area: 'header';
+  background-color: ${props => props.theme.colors.secondary};
 `;
-export const Wrapper = styled.div`
-  max-width: 1280px;
-  margin: 0 auto;
-
-  padding: 0 3%;
-
+export const Layers = styled.div`
   height: 100%;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+export const LayerLeft = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 400px;
+`;
+export const SearchBar = styled.div`
+  margin-left: 15px;
+  input {
+    color: ${props => props.theme.colors.text};
+    font-weight: bold;
+    padding: 2px 20px;
+    border-radius: 50rem;
+    background-color: ${props => props.theme.colors.searchbar.primary};
+  }
+`;
+export const LayerMiddle = styled.div`
+  flex: 1;
+  ul {
+    list-style: none;
+    display: flex;
+    li {
+      margin: 0 20px;
+      a {
+        border-radius: 10px;
+        &:hover {
+          background-color: ${props => props.theme.colors.hover.primary};
+        }
+        text-decoration: none;
+        display: block;
+        padding: 5px 10px;
+      }
+    }
+  }
+`;
+export const LayerRight = styled.div`
+  align-items: center;
+  display: flex;
+`;
+export const Box = styled.div`
+  margin: 0 10px;
 `;
