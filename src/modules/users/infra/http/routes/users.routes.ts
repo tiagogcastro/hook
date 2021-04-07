98f8962e158f8765c45f232722db29ee52299a74
Tiaguin061 <tiaguin180@gmail.com>
2021-04-07 18:27:15 -0300

    feat: Adding avatar router

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express';
-// import multer from 'multer';
-// import uploadConfig from '@config/upload';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 
 import UsersController from '../controllers/UsersController';
 
-// import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-// import UserAvatarController from '../controllers/UserAvatarController';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 
 const usersRouter = Router();
 const usersController = new UsersController();
-// const userAvatarController = new UserAvatarController();
-// const upload = multer(uploadConfig);
+
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', usersController.create);
+usersRouter.patch('/avatar',
+  ensureAuthenticated, 
+  upload.single('avatar'),
+  usersController.updateAvatar);
 
 export default usersRouter;
