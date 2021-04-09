7752181eb15e58d09c96393af4c868ee934f43e2
Tiaguin061 <tiaguin180@gmail.com>
2021-04-09 14:56:35 -0300

    feat: Adding fake hash provider and username test

--- src/modules/users/services/tests/CreateUserService.spec.ts
+++ src/modules/users/services/tests/CreateUserService.spec.ts
@@ -1,3 +1,4 @@
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import AppError from '@shared/errors';
 import FakeUsersRepository from '../../repositories/fakes/FakeUsersRepository';
 import CreateUserService from '../CreateUserService';
@@ -5,22 +6,25 @@ import CreateUserService from '../CreateUserService';
 describe('CreateUser', () => {
   it('Should be able to create new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
 
-    const user = await createUser.execute({
+    const create = await createUser.execute({
       email: 'example123@example.com',
       firstname: 'example firstname',
       lastname: 'example lastname',
       password: 'example123',
-      username: '@example123'
+      username: 'example123'
     });
 
-    expect(user).toHaveProperty('id');
+    expect(create.user).toHaveProperty('id');
+    expect(create.token);
   });
 
   it('Should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
 
     await createUser.execute({
       email: 'example1@example.com',
@@ -40,25 +44,27 @@ describe('CreateUser', () => {
     })).rejects.toBeInstanceOf(AppError);
   });
 
-  // it('Should not be able to create a new user with same username from another', async () => {
-  //   const fakeUsersRepository = new FakeUsersRepository()
-  //   const createUser = new CreateUserService(fakeUsersRepository);
+  it('Should not be able to create a new user with same username from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
 
-  //   await createUser.execute({
-  //     email: 'example2@example.com',
-  //     firstname: 'example firstname',
-  //     lastname: 'example lastname',
-  //     password: 'example1',
-  //     username: 'example1'
-  //   });
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
 
-  //   expect(
-  //     createUser.execute({
-  //     email: 'example1@example.com',
-  //     firstname: 'example firstname',
-  //     lastname: 'example lastname',
-  //     password: 'example1',
-  //     username: 'example1'
-  //   })).rejects.toBeInstanceOf(AppError);
-  // });
+    await createUser.execute({
+      email: 'example2@example.com',
+      firstname: 'example firstname',
+      lastname: 'example lastname',
+      password: 'example1',
+      username: 'example1'
+    });
+
+    expect(
+      createUser.execute({
+      email: 'example1@example.com',
+      firstname: 'example firstname',
+      lastname: 'example lastname',
+      password: 'example1',
+      username: 'example1'
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
