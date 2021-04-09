cb5553f07eb30a62ca729a6a398326901df393ab
Tiaguin061 <tiaguin180@gmail.com>
2021-04-09 15:00:26 -0300

    feat: Import providers from users

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,6 @@
 import { container } from 'tsyringe';
 
-// import '@modules/users/providers';
+import '@modules/users/providers';
 import './providers';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
