e9dd39ce337d120f96fc96fc4e5b9744a9debca2
Tiaguin061 <tiaguin180@gmail.com>
2021-04-10 12:49:50 -0300

    feat: Creating user update service and DTO interface

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors';
+import { inject, injectable } from 'tsyringe';
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+}
+
+@injectable()
+class UpdateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+  async execute({id, email, firstname, lastname, username}: IRequest): Promise<User | undefined> {
+    const user = await this.usersRepository.findByid(id);
+
+    if(!user) {
+      throw new AppError('Você não está logado para atualizar as informações', ' 401 unauthorized', 401);
+    }
+
+    const updateUser = await this.usersRepository.update(id, {
+      email, 
+      firstname, 
+      lastname, 
+      username
+    });
+
+    return updateUser;
+  }
+}
+
+export default UpdateUserService
