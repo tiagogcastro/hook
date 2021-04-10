c02237d3ea86aa1e4e903dfe0ebc0c9f3fb6cb54
Tiaguin061 <tiaguin180@gmail.com>
2021-04-10 12:50:44 -0300

    feat: Creating new methods to user

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -2,12 +2,19 @@
 // No typeorm só vai depender dessa interface.
 
 import ICreateUserDto from '../dtos/ICreateUserDTO';
+import IUpdateUserDto from '../dtos/IUpdateUserDTO';
 import User from '../infra/typeorm/entities/User';
 
 export default interface IUsersRepository {
   create(data: ICreateUserDto): Promise<User>;
+  update(id: string, data: IUpdateUserDto): Promise<User | undefined>;
+  delete(id: string): Promise<void>;
+  show(id: string): Promise<User | undefined>;
+
+  save(user: User): Promise<User>;
+  
   findByEmail(email: string): Promise<User | undefined>;
   findByUsername(username: string): Promise<User | undefined>;
   findByid(id: string): Promise<User | undefined>;
-  save(user: User): Promise<User>;
+
 }
