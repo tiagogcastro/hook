99f48b887e445040cd4a55bbd30ac1053c877064
Tiaguin061 <tiaguin180@gmail.com>
2021-04-14 15:54:53 -0300

    feat: Adding inject dependency to posts

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,4 +6,8 @@ import './providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IPostsRepository from '@modules/posts/repositories/IPostsRepository';
+import PostsRepository from '@modules/posts/infra/typeorm/repositories/PostsRepository';
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+container.registerSingleton<IPostsRepository>('PostsRepository', PostsRepository);
