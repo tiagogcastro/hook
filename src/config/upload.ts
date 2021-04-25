c81ed41e4e412b73c266f0b3f840b03257c8251b
Tiaguin061 <tiaguin180@gmail.com>
2021-04-25 15:03:56 -0300

    refactor: Added date.now on create filename

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,19 @@
 import multer from 'multer'
 import path from 'path'
 import crypto from 'crypto'
+import AppError from '@shared/errors';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
 	tmpFolder,
 	storage: multer.diskStorage({
-		destination: tmpFolder,
+		destination: tmpFolder, 
 		filename: (req, file, callback) => {
-			const fileHash = crypto.randomBytes(10).toString('hex');
-			const fileName = `${fileHash}-${file.originalname}`;
+			const randomHash = crypto.randomBytes(10).toString('hex');
+			const fileName = `${Date.now().toString()}-${randomHash}-${file.originalname}`;
 
-			callback(null, fileName);
+			return callback(null, fileName);
 		}
 	})
 }
