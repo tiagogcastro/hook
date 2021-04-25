8a86618d7f3061aed14c808698198ea25ad85f10
Tiaguin061 <tiaguin180@gmail.com>
2021-04-25 15:08:40 -0300

    feat: Added index on user controller

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import CreateUserService from '@modules/users/services/CreateUserService';
 import DeleteUserService from '@modules/users/services/DeleteUserService';
+import IndexUserService from '@modules/users/services/IndexUserService';
 import ShowUserService from '@modules/users/services/ShowUserService';
 import UpdateUserService from '@modules/users/services/UpdateUserService';
 import { Request, Response } from 'express';
@@ -58,6 +59,15 @@ class UsersController {
 
     return response.json(user);
   }
+
+  async index(request: Request, response: Response): Promise<Response> {
+    const indexUserService = container.resolve(IndexUserService);
+
+    const posts = await indexUserService.execute();
+
+    return response.json(posts);
+  }
+  
 } 
 
 export default UsersController;
