ec667442d2360867a42eae3b7b058d6967df40b5
x14102004x <marcosproenca144@gmail.com>
2021-04-26 22:36:41 -0300

    feat: coverge component have been created

---
+++ src/components/Converge/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Converge: React.FC = ({ children }): JSX.Element => {
+  return <Container>{children}</Container>;
+};
+
+export default Converge;
