56cb2e6bc29f631c172ae11db5adf177db0a1284
x14102004x <marcosproenca144@gmail.com>
2021-04-26 22:37:43 -0300

    feat: the style of the converge component was added

---
+++ src/components/Converge/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 0 3%;
+  padding-left: 115px;
+  @media screen and (max-width: 830px) {
+    padding-left: 3%;
+  }
+  margin: 0 auto;
+  max-width: 1500px;
+  height: 100%;
+`;
