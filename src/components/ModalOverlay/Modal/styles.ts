95633b155174376963b7e20f6ff6150b9a92594a
x14102004x <marcosproenca144@gmail.com>
2021-04-27 22:58:33 -0300

    feat: modal stylization file was added

---
+++ src/components/ModalOverlay/Modal/styles.ts
@@ -0,0 +1,41 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isVisible: boolean;
+}
+export const Container = styled.div<ContainerProps>`
+  position: absolute;
+
+  border-radius: 10px;
+  right: 0;
+  top: 50px;
+  width: 300px;
+  z-index: 3;
+  padding: 40px 50px 100px 18px;
+  background-color: ${props => props.theme.colors.background.ternary};
+  ${props =>
+    !props.isVisible &&
+    css`
+      display: none;
+    `}
+  > button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    background-color: ${props => props.theme.colors.background.ternary};
+    right: 11px;
+    top: -10px;
+    width: 20px;
+    height: 20px;
+
+    transform: rotateZ(-45deg);
+  }
+`;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+`;
