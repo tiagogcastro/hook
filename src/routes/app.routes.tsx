5985e326ce7c09d221d867f400eafb5d8d197e9c
x14102004x <marcosproenca144@gmail.com>
2021-04-28 21:33:07 -0300

    feat: app routes was restructured

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,21 +1,21 @@
-import { ToggleThemeContext } from 'contexts/ToggleThemeProvider';
-import React, { useContext } from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from 'styles/GlobalStyle';
-import Home from '../pages/Home';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import Watch from 'pages/Watch';
+import Profile from 'pages/Profile';
+import Groups from 'pages/Groups';
 import PrivateRoute from './private.routes';
 
 const AppRoutes: React.FC = (): JSX.Element => {
-  const { theme } = useContext(ToggleThemeContext);
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Switch>
-          <PrivateRoute path="/" component={Home} />
-        </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+        <PrivateRoute path="/watch" component={Watch} />
+        <PrivateRoute path="/groups" component={Groups} />
+      </Switch>
     </BrowserRouter>
   );
 };
