d3f5d9a15ce674bc273edc02a5c703db676616b0
x14102004x <marcosproenca144@gmail.com>
2021-04-29 18:48:36 -0300

    feat: the typing of the application themes were modified

--- src/themes/styled.d.ts
+++ src/themes/styled.d.ts
@@ -13,6 +13,7 @@ declare module 'styled-components' {
       background: {
         primary: string;
         secondary: string;
+        ternary: string;
       };
       avatar: string;
       hover: {
