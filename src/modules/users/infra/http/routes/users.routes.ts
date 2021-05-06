2736373511621fe879af2d41a056b93c988fc2a8
Tiaguin061 <tiaguin180@gmail.com>
2021-05-06 18:08:38 -0300

    feat: Added feed router

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import UsersController from '../controllers/UsersController';
+import ProfileController from '../controllers/ProfileController';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
+const profileController = new ProfileController();
 
 usersRouter.post('/', usersController.create);
+usersRouter.get('/feed', ensureAuthenticated, profileController.index);
 
 export default usersRouter;
