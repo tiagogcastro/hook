70d8d864ea38a614eafa154d330dac9431ffca41
Tiaguin061 <tiaguin180@gmail.com>
2021-05-06 18:12:37 -0300

    feat: Added profile router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router} from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+// import friendsRouter from '@modules/friends/infra/http/routes/friends.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import profileRouter from '@modules/users/infra/http/routes/userProfile.routes';
 import postsRouter from '@modules/posts/infra/http/routes/posts.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+// routes.use('/friends', friendsRouter);
+routes.use('/profile', profileRouter);
 routes.use('/', sessionsRouter);
 routes.use('/posts', postsRouter);
 
