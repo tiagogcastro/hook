031808a9620cef028ef28ce2c7287a24f760d6b1
Tiaguin061 <tiaguin180@gmail.com>
2021-05-06 17:38:28 -0300

    refactor: Removed routes related to the user's profile

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,33 +1,11 @@
 import { Router } from 'express';
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import multer from 'multer';
-import uploadConfig from '@config/upload';
 
 import UsersController from '../controllers/UsersController';
-import UsersAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
-const usersAvatarController = new UsersAvatarController();
-
-const upload = multer(uploadConfig);
 
 usersRouter.post('/', usersController.create);
-usersRouter.get('/profile/:id', ensureAuthenticated, usersController.show);
-
-usersRouter.get('/feed', ensureAuthenticated, usersController.index);
-
-usersRouter.put('/', ensureAuthenticated,  usersController.update);
-usersRouter.delete('/', ensureAuthenticated,  usersController.delete);
-
-usersRouter.patch('/avatar',
-  ensureAuthenticated, 
-  upload.single('avatar'),
-  usersAvatarController.update);
-
-usersRouter.post('/covers',
-  ensureAuthenticated, 
-  upload.array('covers', 4), (request) => console.log(request.files));
 
 export default usersRouter;
