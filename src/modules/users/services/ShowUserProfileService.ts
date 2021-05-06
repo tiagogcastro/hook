2b03a5935183113415dda91b17f1855edc0ad043
Tiaguin061 <tiaguin180@gmail.com>
2021-05-06 18:07:56 -0300

    fix: Updated totalPosts to count posts

--- src/modules/users/services/ShowUserProfileService.ts
+++ src/modules/users/services/ShowUserProfileService.ts
@@ -7,7 +7,7 @@ import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IDataReturn {
   user: User;
-  posts: Post[];
+  posts: Post[] | undefined;
   totalPosts: number;
 }
 
@@ -22,13 +22,16 @@ class ShowUserService {
   ) {}
   async execute(id: string): Promise<IDataReturn> {
     const user = await this.usersRepository.show(id);
-    const posts = await this.postsRepository.findAll_OneUser(id);
+    const posts = await this.postsRepository.findAllByUser(id);
 
     if(!user) {
       throw new AppError('Este usuário não existe', '401 unauthorized', 401);
     }
 
-    const totalPosts = posts.length;
+    let totalPosts: number = 0;
+    if(posts?.length) { 
+      totalPosts = posts.length;
+    }
 
     return {
       user,
