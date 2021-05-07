d1142886e45fdbc6800bdc5150d16008dad2e5a8
Tiaguin061 <tiaguin180@gmail.com>
2021-05-07 12:40:01 -0300

    refactor: Changed id to user_id an params

--- src/modules/users/services/ShowUserProfileService.ts
+++ src/modules/users/services/ShowUserProfileService.ts
@@ -20,14 +20,14 @@ class ShowUserService {
     @inject('PostsRepository')
     private postsRepository: IPostsRepository,
   ) {}
-  async execute(id: string): Promise<IDataReturn> {
-    const user = await this.usersRepository.show(id);
-    const posts = await this.postsRepository.findAllByUser(id);
+  async execute(user_id: string): Promise<IDataReturn> {
+    const user = await this.usersRepository.findByid(user_id);
+    const posts = await this.postsRepository.findAllByUser(user_id);
+    console.log(user);
 
     if(!user) {
       throw new AppError('Este usuário não existe', '401 unauthorized', 401);
     }
-
     let totalPosts: number = 0;
     if(posts?.length) { 
       totalPosts = posts.length;
