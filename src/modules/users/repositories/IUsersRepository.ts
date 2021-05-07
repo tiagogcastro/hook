1c1e1a2f7bbac323fe3b4e5990708bde49ddb7bb
Tiaguin061 <tiaguin180@gmail.com>
2021-05-07 12:42:58 -0300

    refactor: Removed show method

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -9,13 +9,12 @@ export default interface IUsersRepository {
   create(data: ICreateUserDto): Promise<User>;
   update(id: string, data: IUpdateUserDto): Promise<User | undefined>;
   delete(id: string): Promise<void>;
-  show(id: string): Promise<User | undefined>;
 
   save(user: User): Promise<User>;
   
   findByEmail(email: string): Promise<User | undefined>;
   findByUsername(username: string): Promise<User | undefined>;
-  findByid(id: string): Promise<User | undefined>;
+  findByid(user_id: string): Promise<User | undefined>;
 
   findAll(): Promise<User[]>;
 
