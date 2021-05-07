73aa26de11e1ebc5f14a32a616c07eef962ce603
Tiaguin061 <tiaguin180@gmail.com>
2021-05-07 12:40:33 -0300

    feat: Using ensureAuthenticate for all profile router

--- src/modules/users/infra/http/routes/userProfile.routes.ts
+++ src/modules/users/infra/http/routes/userProfile.routes.ts
@@ -7,24 +7,25 @@ import ProfileController from '../controllers/ProfileController';
 import UsersAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
+usersRouter.use(ensureAuthenticated)
 
 const profileController = new ProfileController();
 const usersAvatarController = new UsersAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.get('/:id', ensureAuthenticated, profileController.show);
+usersRouter.get('/:user_id', profileController.show);
 
-usersRouter.put('/', ensureAuthenticated,  profileController.update);
-usersRouter.delete('/', ensureAuthenticated,  profileController.delete);
+usersRouter.put('/', profileController.update);
+usersRouter.delete('/', profileController.delete);
 
 usersRouter.patch('/avatar',
-  ensureAuthenticated, 
   upload.single('avatar'),
-  usersAvatarController.update);
+  usersAvatarController.update
+);
 
 usersRouter.post('/covers',
-  ensureAuthenticated, 
-  upload.array('covers', 4), (request) => console.log(request.files));
+  upload.array('covers', 4), (request) => console.log(request.files)
+);
 
 export default usersRouter;
