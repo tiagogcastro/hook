d31e963654c74db8f89f615403a296d981433d9c
Tiaguin061 <tiaguin180@gmail.com>
2021-05-22 20:29:53 -0300

    feat: Using inject dependency at providers

---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,8 @@
+import { container } from 'tsyringe';
+import EtherealMailProvider from './implementations/EthrealMailProvider';
+import IMailProvider from './models/IMailProvider';
+
+container.registerSingleton<IMailProvider>(
+  'MailProvider',
+  EtherealMailProvider
+);
