3829ec3fbe9224d49e38d4c17d5fca2da3d44634
Tiaguin061 <tiaguin180@gmail.com>
2021-05-22 20:31:04 -0300

    refactor: Importing inject dependency at providers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,3 @@
-import { container } from 'tsyringe';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider
-);
+import './MailProvider';
+import './MailTemplateProvider';
+import './StorageProvider';
