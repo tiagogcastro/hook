3d74e3ef09b3d3334adafa631e8ef2d0a5b3cd6a
x14102004x <marcosproenca144@gmail.com>
2021-05-26 21:05:19 -0300

    feat: the groups container was added

--- src/pages/Groups/index.tsx
+++ src/pages/Groups/index.tsx
@@ -1,16 +1,39 @@
+import AccessibilityGroup from 'components/AccessibilityGroup';
 import Header from 'components/Header';
 import React from 'react';
-import { Body, Container } from './styles';
+import { FiPlus } from 'react-icons/fi';
+import { useTheme } from 'styled-components';
+import {
+  Container,
+  Content,
+  Group,
+  GroupNest,
+  GroupsContainer,
+  GroupSettings
+} from './styles';
 
-const Groups: React.FC = (): JSX.Element => {
+const Home: React.FC = (): JSX.Element => {
+  const { colors } = useTheme();
   return (
     <Container>
       <Header />
-      <Body>
+      <GroupsContainer>
+        <GroupNest>
+          <Group />
+          <Group />
+        </GroupNest>
+        <GroupSettings>
+          <button type="button">
+            <FiPlus size={50} color={colors.icons.ternary} />
+          </button>
+        </GroupSettings>
+      </GroupsContainer>
+      <AccessibilityGroup type="group" />
+      <Content>
         <div style={{ height: '1000vh' }} />
-      </Body>
+      </Content>
     </Container>
   );
 };
 
-export default Groups;
+export default Home;
