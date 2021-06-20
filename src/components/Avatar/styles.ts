41056ccdbac264bfa7dfacd9e732bc8100896b6a
pprograminha <marcosproenca144@gmail.com>
2021-06-20 19:36:34 -0300

    refactor: the avatar style was reworked because of the default image

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -4,8 +4,15 @@ interface ContainerProps {
   size: number;
 }
 export const Container = styled.div<ContainerProps>`
+  position: relative;
+  z-index: 3;
   background-color: ${props => props.theme.colors.avatar};
   border-radius: 50rem;
   width: ${props => props.size}px;
   height: ${props => props.size}px;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
