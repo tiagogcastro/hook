c3cb8ea915e83122f19cc32f2ce6f2ba036843de
pprograminha <marcosproenca144@gmail.com>
2021-06-20 19:42:27 -0300

    refactor: in converge component a new feature was added

--- src/components/Converge/index.tsx
+++ src/components/Converge/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Container } from './styles';
 
-const Converge: React.FC = ({ children }): JSX.Element => {
-  return <Container>{children}</Container>;
+interface IConvergeProps {
+  width?: number;
+}
+const Converge: React.FC<IConvergeProps> = ({
+  children,
+  width
+}): JSX.Element => {
+  return <Container width={width || 1500}>{children}</Container>;
 };
 
 export default Converge;
